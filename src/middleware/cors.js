2870e738cf6a5f84439e3c7ebe8c4ccc827509c0
zainab-kassim <kassimzainab11@gmail.com>
2024-10-10 14:32:53 +0100

    testing

--- src/middleware/cors.js
+++ src/middleware/cors.js
@@ -1,7 +1,9 @@
 import cors from 'cors'
 // CORS setup
 const corsMiddleware = cors({
-  origin: 'https://brobl-client.vercel.app'|| 'http://localhost:3000', // Replace with your client's origin if needed
+  origin: process.env.NODE_ENV === 'production'
+    ? 'https://brobl-client.vercel.app'
+    : 'http://localhost:3000', // Replace with your client's origin if needed
   methods: ['GET', 'PUT', 'POST', 'DELETE'],
   optionsSuccessStatus: 200, // For legacy browsers
 });
